eedb4883a09df29fbee79c2703f4e004dc14e8dc
ellefaraday <faraday.elle@gmail.com>
2017-09-22 23:53:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 const app = "I don't do much."
+
 function destructivelyAppendKitten(name){
   kittens.push(name)
 }
 
 function destructivelyPrependKitten(name){
   kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function appendKitten(name){
+  kittens.append(name)
+}
+
+function prependKitten(name){
+  kittens.splice(0,name)
+}
+
+function removeLastKitten(){
+  kittens.slice(0,kittens.length-1)
+}
+
+function removeFirstKittne(){
+  kittens.slice(1,kittens.length)
 }
